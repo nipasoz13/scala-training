375f0c01c9a10b87e70d76d82f340369bb5b0f7f
Nicolas Pascal <nicolas.pascal.job@gmail.com>
2017-05-21 19:23:58 +1000

    Chapter 4, Maps, Exercise 2

--- src/main/scala/book/impatient/Chapter4_Maps.scala
+++ src/main/scala/book/impatient/Chapter4_Maps.scala
@@ -1,5 +1,9 @@
 package book.impatient
 
+import java.nio.file.Path
+
+import scala.collection.mutable
+
 /**
   * Created by npa on 21/05/17.
   */
@@ -11,5 +15,18 @@ object Chapter4_Maps {
     */
   def getDiscount(originalPrices: Map[String, BigDecimal]): Map[String, BigDecimal] = {
     for ((name, price) <- originalPrices) yield (name, price - price / 10)
+  }
+
+  /** 2. Write a program that reads words from a file. Use a mutable map to count
+    * how often each word appears. To read the words, simply use a java.util.Scanner
+    */
+  def wordCount(filePath: Path): Map[String, Int] = {
+    val in = new java.util.Scanner(filePath)
+    val wordCount = mutable.Map[String, Int]().withDefaultValue(0)
+    while (in.hasNext()) {
+      val word = in.next().capitalize
+      wordCount += (word -> (wordCount(word) + 1))
+    }
+    wordCount.toMap.withDefaultValue(0)
   }
 }
--- src/test/scala-2.12/book/impatient/Chapter4Test.scala
+++ src/test/scala-2.12/book/impatient/Chapter4Test.scala
@@ -1,7 +1,10 @@
 package book.impatient
 
+import java.nio.file.{Path, Paths}
+
 import org.scalatest.{FunSpec, Matchers}
 import Chapter4_Maps._
+
 
 /**
   * Created by npa on 21/05/17.
@@ -12,11 +15,25 @@ class Chapter4Test extends FunSpec with Matchers {
       it("should create a discount of 10%") {
         val originalPrices = Map("Item 1" -> BigDecimal(10.5), "Item 2" -> BigDecimal(150.2))
         val discountedPrices = getDiscount(originalPrices)
-        discountedPrices should have size(2)
+        discountedPrices should have size 2
         discountedPrices("Item 1") should be(BigDecimal(9.45))
         discountedPrices("Item 2") should be(BigDecimal(135.18))
       }
     }
+
+    describe("Exercise 2") {
+      it("should count words of a file") {
+        val testFilePath = getFilePathFromClassPath("wordCountTest.txt")
+        val wordCountResult = wordCount(testFilePath)
+        wordCountResult("For") should be(2)
+        wordCountResult("Test") should be(2)
+        wordCountResult("Hello") should be(1)
+        wordCountResult("Java") should be(0)
+      }
+    }
   }
 
+  def getFilePathFromClassPath(filePath: String): Path = {
+    Paths.get(getClass.getClassLoader.getResource(filePath).getPath)
+  }
 }
