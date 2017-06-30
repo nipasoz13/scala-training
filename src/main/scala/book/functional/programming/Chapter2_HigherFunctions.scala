7b61dc9ed7f10bc85c05648b51fb28a17fc1a985
Nicolas Pascal <nicolas.pascal.job@gmail.com>
2017-06-30 13:18:34 +1000

    Functional programming, High ordered functions, 2.2 isOrdered

--- src/main/scala/book/functional/programming/Chapter2_HigherFunctions.scala
+++ src/main/scala/book/functional/programming/Chapter2_HigherFunctions.scala
@@ -4,6 +4,9 @@ package book.functional.programming
   * Created by npa on 26/06/17.
   */
 object Chapter2_HigherFunctions {
+  /**
+    * Exercise 2.1: Write a recursive function to get the nth Fibonacci number
+    */
   def fib(n: Int): Int = {
     @annotation.tailrec
     def go(n: Int, prev: Int, next: Int): Int = n match {
@@ -13,5 +16,19 @@ object Chapter2_HigherFunctions {
     }
 
     go(n, 0, 1)
+  }
+
+  /**
+    * Exercise 2.2: Implement isSorted , which checks whether an Array[A] is sorted according to a
+    * given comparison function:
+    */
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def isSorted(list: Array[A], prev: A, isSortedSoFar: Boolean): Boolean = {
+      if (list.isEmpty) isSortedSoFar
+      else if(!isSortedSoFar) false
+      else isSorted(list.tail, list.head, isSortedSoFar && ordered(prev, list.head))
+    }
+    isSorted(as, as.head, true)
   }
 }
--- src/test/scala-2.12/functional/programming/Chapter2_HigherFunctions_Test.scala
+++ src/test/scala-2.12/functional/programming/Chapter2_HigherFunctions_Test.scala
@@ -1,19 +1,31 @@
 package functional.programming
 
 import book.functional.programming.Chapter2_HigherFunctions._
-import book.impatient.Chapter5_Class.Student
 import org.scalatest.{FunSpec, Matchers}
+
 /**
   * Created by npa on 26/06/17.
   */
 class Chapter2_HigherFunctions_Test extends FunSpec with Matchers {
   describe("Chapter 2") {
-    describe("Exercise 1: Write loops recursively") {
+    describe("Exercise 2.1") {
       it("Should produce Fibonacci numbers") {
         fib(0) should be(0)
         fib(1) should be(1)
         fib(4) should be(3)
         fib(7) should be(13)
+      }
+    }
+
+    describe("Exercise 2.2") {
+      it("should return if a array is sorted according to a comparison") {
+        val supOrEquals = (x: Int, y: Int) => x >= y
+        isSorted(Array(5, 4, 3, 2, 1), supOrEquals) should be(true)
+        isSorted(Array(5, 4, 10, 13), supOrEquals) should be(false)
+
+        val infOrEquals = (x: String, y: String) => x <= y
+        isSorted(Array("a","b","c","d"), infOrEquals) should be(true)
+        isSorted(Array("d","a","c","d"), infOrEquals) should be(false)
       }
     }
   }
